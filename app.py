c5334a5ab9564f1cb90df4ec7285515a0ea9b428
Infvmous <alexeyheather@gmail.com>
2020-09-27 03:30:21 +0700

    code re-factoring

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
 import json
 
 
-names = ['Андрей', 'Елена', 'Юлия', 'Роберт', 'Валерия', 'Амбер']
-male_names = []
-female_names = []
+def json_to_dict(filename):
+    with open(filename, encoding='utf-8-sig') as f:
+        return json.load(f)
 
-filename = 'russian_names.json'
 
-with open(filename, encoding='utf-8-sig') as f:
-    data = json.load(f)
+def sort(names, all_names):
+    male_names = []
+    female_names = []
+    for dicti in all_names:
+        name = dicti['Name']
+        if name in names:
+            if dicti['Sex'] == 'Ж':
+                female_names.append(name)
+            else:
+                male_names.append(name)
+    return male_names, female_names
 
-for dicti in data:
-    name = dicti['Name']
-    if name in names:
-        if dicti['Sex'] == 'Ж':
-            female_names.append(name)
-        else:
-            male_names.append(name)
 
-print(male_names)
-print(female_names)
+def main():
+    names = ['Андрей', 'Елена', 'Юлия', 'Роберт', 'Валерия', 'Амбер']
+    filename = 'russian_names.json'
+
+    all_names = json_to_dict(filename)
+    sorted_names = sort(names, all_names)
+
+    print(sorted_names[0])
+    print(sorted_names[1])
+
+
+if __name__ == '__main__':
+    main()
